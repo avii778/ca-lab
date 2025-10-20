043a615e2c4ea903bb2dda1a01d9c3549b68bb8e
Felcita606 <michellezhou06@hotmail.com>
2025-10-20 16:14:44 -0400

    logoutcontroller implemented

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,6 +1,9 @@
 package interface_adapter.logout;
 
 import use_case.logout.LogoutInputBoundary;
+import use_case.logout.LogoutInteractor;
+import use_case.logout.LogoutOutputData;
+import use_case.signup.SignupInputData;
 
 /**
  * The controller for the Logout Use Case.
@@ -10,13 +13,15 @@ public class LogoutController {
     private LogoutInputBoundary logoutUseCaseInteractor;
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
-        // TODO: Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
      * Executes the Logout Use Case.
      */
-    public void execute() {
-        // TODO: run the use case interactor for the logout use case
+    public void execute(String username) {
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username);
+
+        LogoutInteractor.execute(logoutOutputData);
     }
 }
